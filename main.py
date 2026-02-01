3b53bc2a2d533410e7ce1f210f1dee14182f1029
George Justus Morara <george@Georges-MacBook-Pro.local>
2026-02-01 18:22:44 +0000

    Fix pipeline order and add EDA stage to main orchestrator

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src import data_loader, feature_engineering, modeling, visualization
 from src.statistical_analysis import run_statistical_analysis
+from src import eda
 
 def run_pipeline():
     print("Starting Unilever Pricing Analytics Pipeline...\n")
@@ -7,25 +8,35 @@ def run_pipeline():
     # Step 1: Merge raw data
     print("Step 1 — Merging raw data...")
     merged_df = data_loader.merge_data()
+    enriched_df = feature_engineering.engineer_features(merged_df)
     print(f"Data merged: {merged_df.shape[0]} rows, {merged_df.shape[1]} columns\n")
 
+    # Step 1.1 : Run EDA
+        
+    print("step 1.1 - Exploratory Data Analysis (EDA)...")
+    df = eda.run_eda(merged_df)
+
     # Step 2: Feature engineering
+
     print("Step 2 — Feature engineering...")
     enriched_df = feature_engineering.engineer_features(merged_df)
     print(f"Features engineered: {enriched_df.shape[0]} rows, {enriched_df.shape[1]} columns\n")
     print(enriched_df.head(), "\n")
 
     # Step 3: Modeling
+
     print("Step 3 — Running machine learning models...")
     modeling.run_models(enriched_df)
     print("Machine learning step complete\n")
 
     # Step 4: Statistical analysis
+
     print("Step 4 — Statistical Validation (SciPy)...")
     run_statistical_analysis()
 
 
     # Step 5: Visualization
+
     print("Step 5 — Creating executive charts...")
     visualization.plot_price_by_region()
     visualization.plot_price_vs_brand_strength()
